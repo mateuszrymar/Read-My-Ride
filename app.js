63c7ba812cd4be9fb97632254803201430d792aa
mateuszrymar <118372766+mateuszrymar@users.noreply.github.com>
2022-12-27 19:06:30 +0100

    GPX parsing using RegEx

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const gpxFile = "Strava.gpx";
+const gpxFile = "Routes.gpx";
 const readGpxBtn = document.getElementById("read-gpx-btn");
 const rawText = document.getElementById("gpx-raw");
 
 let gpxText;
 let parser;
-let xmlFile = new XMLHttpRequest();
+let xhr = new XMLHttpRequest();
+let trackPointStart = /<trkpt/;
+let trackPointEnd = /<\/trkpt>/;
+let trackPoint = /(<trkpt)((.|\n)*?)(<\/trkpt>)/g;
+let trackPointList;
+
 
 readGpxBtn.addEventListener("click", readGpx);
 
 function readGpx() {
-  xmlFile.open("GET", gpxFile, false);
-  xmlFile.send();
-  rawText.innerHTML = xmlFile.responseText;
+  xhr.open("GET", gpxFile, false);
+  xhr.send();
+
+	rawText.innerHTML = xhr.responseText;
+
+	trackPointList = xhr.responseText.match(trackPoint);
+	// const result = trackPoint.exec(trackPointList);
+	console.log(trackPointList);	
 }
+
+
+// function extractTrackPoints(stringToProcess, stringStart, stringFinish) {
+// 	return stringToProcess.match()
+// }
+
