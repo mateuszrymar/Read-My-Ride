77adbf37b82a35b1ad07bb24805e5f92b6061952
mateuszrymar <118372766+mateuszrymar@users.noreply.github.com>
2023-01-12 15:54:36 +0100

    Looks like we're almost there...

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,18 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',  
   output: {
-    filename: '[name][contenthash].js',
+    filename: 'index_bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true
   },
   module: {
     rules: [
@@ -28,12 +37,17 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif|zip|gpx|ico)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'src/index.html'
+      template: './src/index.html',
+      favicon: './src/images/favicon.ico'
     }),
   ],
 };
